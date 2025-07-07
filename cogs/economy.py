98d310d002802ea1e92081c19a8aa32b73676dca
Henry <cootshk@duck.com>
2025-07-07 18:36:27 -0400

    cleanup economy through win

--- cogs/economy.py
+++ cogs/economy.py
@@ -52,30 +52,20 @@ class EconomyCog(commands.Cog):
     def __save_bank(self):
         with open(self.ECONOMY_FILE, "w") as s:
             json.dump(self.bank, s, indent=4)
-    def set(self, user: str, coins: int):
+    def add(self, user: MemberLike, coins: int):
+        """Adds coins to a user's balance."""
+        user_id = self.__get_id(user)
+        self.set(user_id, self.get(user_id) + coins)
+    def sub(self, user: MemberLike, coins: int):
+        return self.add(user, -coins)
+    def set(self, user: MemberLike, coins: int):
         """Sets the balance of a user."""
-        self.bank[user] = coins
+        self.bank[self.__get_id(user)] = coins
         self.__save_bank()
-    @overload
-    def get(self, user: str) -> int:
-        """Gets the balance of a user."""
-        ...
-    @overload
-    def get(self, user: Member) -> int:
-        """Gets the balance of a user."""
-        ...
-    @overload
-    def get(self, user: discord.User | discord.Member) -> int:
-        """Gets the balance of a user."""
-        ...
-    @overload
-    def get(self, user: int) -> int:
-        """Gets the balance of a user."""
-        ...
     def get(self, user: MemberLike) -> int:
         """Gets the balance of a user."""
         user_id = self.__get_id(user)
-        if self.bank[user_id]:
+        if user_id in self.bank:
             return self.bank[user_id]
         else:
             self.bank[user_id] = 0
@@ -90,7 +80,7 @@ class EconomyCog(commands.Cog):
             'You did a great job and earned', 
             'You exploited a citizen and earned', 
             'You forfeited your evening to the mods and earned', 
-            'You stole', 
+            # 'You stole', 
             'You were such a cutie you got', 
             'You begged and got', 
             'You sent your nudes to the mods and were paid', 
@@ -106,23 +96,8 @@ class EconomyCog(commands.Cog):
             'You "found"',
         ]
         # TODO: fix
-        for current_acc in self.bank['users']:
-            if ctx.author.name == current_acc['name']:
-                coins = int(current_acc['balance'])
-                earn = random.randint(1,3001)
-                newcoins = coins + earn
-                self.set(ctx.author.name, newcoins)
-                if earn == 1984:
-                    await ctx.send("Your speaking privileges have been revoked")
-                    newtime = newtime = datetime.timedelta(minutes=int("5"))
-                    await ctx.author.edit(timed_out_until=discord.utils.utcnow() + newtime) # type: ignore
-                else:    
-                    await ctx.send(f"{random.choice(responses)} {earn} eden coins")
-                break
-        if not found:
-            earn = random.randint(1,3000)
-            self.set(ctx.author.name, earn)
-            await ctx.send(f"{random.choice(responses)} {earn} eden coins")
+        self.add(ctx.author, coins:=(random.randint(1, 3000)))
+        await ctx.send(f"{random.choice(responses)} {coins} eden coins")
 
         self.__save_bank()
 
@@ -131,24 +106,10 @@ class EconomyCog(commands.Cog):
         if isinstance(error, commands.CommandInvokeError):
             await ctx.send("Please make an account with `;bal` first.")
     # Economy commands
-    @commands.command()
+    @commands.command(name='bal')
     async def bal(self, ctx: commands.Context, user: Optional[Member] = None): # type: ignore
-        if user is None:
-            user: Member = ctx.author # type: ignore
-            pronoun = "Your"
-        else:
-            pronoun = f"{user.mention}'s"
-        for current_acc in self.bank['users']:
-            if user.name == current_acc['name']:
-                await ctx.send(f"{pronoun} balance is {current_acc['balance']} eden coins")
-                break
-        else:
-            self.bank['users'].append({
-                'name': user.name,
-                'balance': 0
-            })
-            await ctx.send(f"{pronoun} balance is 0 eden coins")
-            self.__save_bank()
+        await ctx.send(f"{user.mention + '\'s' if user else 'Your'} balance is {self.get(user or ctx.author)} eden coins.")
+        
 
     @commands.command(name="topbal")
     async def topbal(self, ctx: commands.Context):
@@ -159,7 +120,7 @@ class EconomyCog(commands.Cog):
                 return
 
             # Sort users by balance (highest first)
-            sorted_users = sorted(self.bank['users'], key=lambda x: x['balance'], reverse=True)
+            sorted_users = sorted(self.bank.items(), key=lambda x: x[1], reverse=True)
 
             # Limit to top 10
             top_users = sorted_users[:100]
@@ -170,8 +131,8 @@ class EconomyCog(commands.Cog):
             # Create paginated embeds
             for i in range(0, len(top_users), 10):  # Show 10 users per page
                 embed = discord.Embed(title="Economy Leaderboard", color=discord.Color.gold())
-                for idx, (user) in enumerate(top_users[i:i+10], start=i+1):
-                    embed.add_field(name=f"{idx}. {user['name']}", value=f"{user['balance']} eden coins", inline=False)
+                for idx, (user_id, balance) in enumerate(top_users[i:i+10], start=i+1):
+                    embed.add_field(name=f"{idx}. <@{user_id}>", value=f"{balance} eden coins", inline=False)
 
                 paginator.add_page(embed)
 
@@ -183,62 +144,31 @@ class EconomyCog(commands.Cog):
     @commands.command(name='coinflip', aliases=['cf'])
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def coinflip(self, ctx: commands.Context, *, txt: str):
-        found = False
+        earn = 1000
         sides = ['heads', 'tails']
         toss = random.choice(sides)
 
         if txt.lower() in sides:
             if txt.lower() == toss:
-                earn = 1000
-                for current_acc in self.bank['users']:
-                    if ctx.author.name == current_acc['name']:
-                            found = True
-                            coins = int(current_acc['balance'])
-                            newcoins = coins + earn
-                            self.set(ctx.author.name, newcoins)
-                            await ctx.send(f"You won {earn} eden coins")
-                            break
-
-                if not found:
-                    self.set(ctx.author.name, earn)
-                    await ctx.send(f"You won {earn} eden coins")
-
+                self.add(ctx.author, earn)
+                await ctx.send(f"You won {earn} eden coins!")
             else:
-                    await ctx.send('You lost')
+                await ctx.send('You lost')
         else:
             await ctx.send('Pick either ``heads`` or ``tails``')
-
-
 
     @commands.command(name='subbal')
     @commands.has_any_role('MODERATOR', 'happy')
     @commands.cooldown(1,500, commands.BucketType.user)
-    async def subbal(self, ctx: commands.Context, member: Member):
-        userid = member.name
-        found = False
-        for current_acc in self.bank['users']:
-            if userid == current_acc['name']:
-                found = True
-                self.set(userid, 0)  # Set balance to 0
-
-                await ctx.send(f"{member.mention} 's balance is {current_acc['balance']} eden coins")
-                break
-        if not found:
-              self.bank['users'].append({
-                  'name': userid,
-                  'balance': 0
-              })
-              await ctx.send(f"{member.mention} 's balance is 0 eden coins")
-
-        self.__save_bank()
-
+    async def subbal(self, ctx: commands.Context, member: MemberLike, amount: int):
+        self.sub(member, amount)
+        await ctx.send(f"{member}'s balance is now {self.get(member)} eden coins")
 
     @commands.command(name='setbal')
     @commands.has_any_role('Bonked by Zi')
-    async def setbal(self, ctx: commands.Context, member: Member, coins: int):
-        userid = member.name
-        self.set(userid, coins)  # Set balance to specified coins
-        await ctx.send(f"{member.mention} 's balance is {coins} eden coins")
+    async def setbal(self, ctx: commands.Context, member: MemberLike, coins: int):
+        self.set(member, coins)  # Set balance to specified coins
+        await ctx.send(f"{member.mention if isinstance(member, Member) else member} 's balance is {coins} eden coins")
 
     @setbal.error
     async def setbal_error(self, ctx: commands.Context, error):
@@ -249,21 +179,13 @@ class EconomyCog(commands.Cog):
     async def win(self, ctx: commands.Context):
         userid = ctx.author.name
         price = 100_000
-        for current_acc in self.bank['users']:
-            if userid == current_acc['name']:
-                coins = int(current_acc['balance'])
-                if coins < price:
-                    await ctx.send(f'You do not have enough coins, you need {price:,}')
-                    break
-                else:
-                    newcoins = coins - price
-                    current_acc['balance'] = newcoins
-                    channel: discord.TextChannel = self.bot.get_channel(CHANNELS.WINNERS) # type: ignore
-                    await channel.send(f'{ctx.author.mention} won the prize')
-                    break
+        if self.get(ctx.author) < price:
+            await ctx.send(f"You do not have enough coins to win. You need at least {price:,} eden coins.")
+            return
         else:
-            self.set(userid, 0)
-            await ctx.send("You do not have an account")
+            self.sub(ctx.author, price)
+            channel: discord.TextChannel = self.bot.get_channel(CHANNELS.WINNERS)  # type: ignore
+            await channel.send(f"{ctx.author.mention} won the prize")
 
 
 

61f8c1a78878dfaa8eb63eae8ecb82b7a885e020
Henry <cootshk@duck.com>
2025-07-10 23:02:21 -0400

    move back to protected users

--- cogs/economy.py
+++ cogs/economy.py
@@ -139,7 +139,7 @@ class EconomyCog(commands.Cog):
             # Limit to top 100 users
             top_users = list(sorted_users)[:100]
             # Remove protected users
-            for user_id in self.protected_role_ids:
+            for user_id in self.protected_users:
                 if str(user_id) in top_users:
                     top_users.remove(str(user_id))
 
@@ -255,7 +255,7 @@ class EconomyCog(commands.Cog):
             self.sub(ctx.author, cost)
             await ctx.send(f'You lost {cost:,} eden coins.')
 
-    protected_role_ids = [ROLES.MODERATOR]
+    protected_users = [USERS.HAPPY, USERS.RAINBOW, USERS.FROST, USERS.SLOINAC, USERS.MASCIAN, USERS.VIC]
     @commands.command(name="steal", aliases=["rob", "heist"])
     @commands.cooldown(1, 7200, commands.BucketType.user)
     async def steal(self, ctx: commands.Context, member: Member):
@@ -264,8 +264,7 @@ class EconomyCog(commands.Cog):
         # Replace with the actual name or ID of the protected role
 
         # Check if the target has the protected role
-        if any(role.id in self.protected_role_ids for role in member.roles):
-    # same protection logic
+        if member.id in self.protected_users:
             await ctx.send(f"{member.display_name} is protected and cannot be stolen from.")
             self.steal.reset_cooldown(ctx)  # type: ignore
             return

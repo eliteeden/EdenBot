0a5ff9ee0f2d5a803a82a90d586ccd5e86cb7dee
Henry <cootshk@duck.com>
2025-07-20 22:52:37 -0400

    development commands in their own cog

--- main.py
+++ main.py
@@ -90,12 +90,10 @@ async def on_ready():
                     print(f"Loaded cog: {filename[:-3]}")
                 except Exception as e:
                     print(f"Failed to load cog {filename[:-3]}: {e}")
-        
-        # TODO: this needs to be a command
-        await bot.tree.sync()
+        bot.cogs["DevCog"].startup = int(datetime.datetime.now().timestamp()) # type: ignore
     except Exception as e:
         print(f"Failed to sync commands: {e}")
-    channel: discord.TextChannel = bot.get_channel(CHANNELS.CAPITAL)  # type: ignore
+    channel: discord.TextChannel = bot.get_channel(CHANNELS.CAPITAL) # type: ignore
     await channel.send("I'm backkkkk")
     print('Systems online')
 
@@ -1311,106 +1309,6 @@ async def districtclaim(ctx, category_id: int):
     except asyncio.TimeoutError:
         # Stop listening after timeout if no reply occurs
         await ctx.send("No replies detected for remaining messages within the timeout period.")
-
-# Cog commands
-@commands.has_any_role(ROLES.TOTALLY_MOD)
-@bot.command()
-async def cogs(ctx: commands.Context):
-    await ctx.send("Loaded cogs: `" + "`, `".join(bot.cogs.keys()) + "`")
-    # List available cogs in the cogs directory
-    cogs = os.listdir("cogs")
-    send_cogs = []
-    for cog in cogs:
-        if cog.endswith(".py"):
-            cog_name = cog[:-3]
-            send_cogs.append(cog_name)
-    await ctx.send("Available cogs: `" + "`, `".join(send_cogs) + "`")
-@cogs.error
-async def cogs_error(ctx: commands.Context, error: commands.CommandError):
-    if isinstance(error, commands.MissingAnyRole):
-        await ctx.send("Trust me, you can't untangle the spaghetti inside.")
-    else:
-        await ctx.send(f"An unexpected error occurred: {error}")
-@bot.command()
-@commands.has_any_role(ROLES.TOTALLY_MOD)
-async def reload(ctx: commands.Context, cog: str):
-    """Reloads a specific cog."""
-    try:
-        await bot.reload_extension(f"cogs.{cog}")
-        await ctx.send(f"{cog} cog reloaded successfully!")
-    except Exception as e:
-        await ctx.send(f"Failed to reload {cog} cog: {e}")
-        print(e)
-@reload.error
-async def reload_error(ctx: commands.Context, error: commands.CommandError):
-    """Handles errors for the reload command."""
-    if isinstance(error, commands.MissingAnyRole):
-        await ctx.send("buzz off butterfingers, only an elite few can tinker with me like that")
-    elif isinstance(error, commands.ExtensionNotFound):
-        await ctx.send("The specified cog does not exist.")
-    elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("What do you want me to do, reload *everything*?")
-    else:
-        await ctx.send(f"An unexpected error occurred: {error}")
-@bot.command()
-@commands.has_any_role(ROLES.TOTALLY_MOD)
-async def load(ctx: commands.Context, cog: str):
-    """Loads a specific cog."""
-    try:
-        await bot.load_extension(f"cogs.{cog}")
-        await ctx.send(f"{cog} cog loaded successfully!")
-    except Exception as e:
-        await ctx.send(f"Failed to load {cog} cog: {e}")
-        print(e)
-@load.error
-async def load_error(ctx: commands.Context, error: commands.CommandError):
-    """Handles errors for the load command."""
-    if isinstance(error, commands.MissingAnyRole):
-        await ctx.send("what do you even mean to do with that command, filthy peasant?")
-    elif isinstance(error, commands.ExtensionNotFound):
-        await ctx.send("The specified cog does not exist.")
-    elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("What even is there to load?")
-    else:
-        await ctx.send(f"An unexpected error occurred: {error}")
-
-@bot.command()
-@commands.has_any_role(ROLES.TOTALLY_MOD)
-async def nohup(ctx):
-    try:
-        with open("nohup.out", "r") as f:
-            await ctx.author.send(f.read()[-1999:])
-    except Exception as e:
-        await ctx.send(f"Error: {e}")
-
-@bot.command()
-@commands.has_any_role(ROLES.TOTALLY_MOD)
-async def pull(ctx: commands.Context):
-    """Fetches the latest changes from git."""
-    try:
-        # Run the git pull command
-        result = await subprocess.create_subprocess_shell("git pull", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        stdout, stderr = await result.communicate()
-        if stdout:
-            print(stdout.decode())
-        if stderr:
-            print(stderr.decode())
-        if result.returncode == 0:
-            await ctx.send("Successfully pulled the latest changes from git.")
-            await ctx.author.send("Git pull output:\n" + stdout.decode())
-        else:
-            await ctx.send(f"Failed to pull changes: {stderr.decode()}")
-    except Exception as e:
-        await ctx.send(f"An error occurred while pulling changes: {e}")
-@pull.error
-async def pull_error(ctx: commands.Context, error: commands.CommandError):
-    """Handles errors for the pull command."""
-    if isinstance(error, commands.MissingAnyRole):
-        await ctx.send("Where are you trying to pull from, the depths of hell? Only the elite can do that.")
-    else:
-        await ctx.send(f"An unexpected error occurred: {error}")
-
 # This was mostly created by Happy!
 load_dotenv()
 

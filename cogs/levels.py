875e647df095e2179e84d616589975dfb2ff3683
HappyJuice3 <elitelydead@outlook.com>
2025-08-08 21:08:16 +0100

    unbound error (ty cootshk)

--- cogs/levels.py
+++ cogs/levels.py
@@ -292,8 +292,10 @@ class Levels(commands.Cog):
             elif member.banner:
                 await member.banner.with_format("png").save(f"/tmp/avatar.png")
                 custom_border = Image.open(f"/tmp/avatar.png").convert("RGBA")
+            else:
+                custom_border = Image.open("media/rank/komi.jpg").convert("RGBA")
         else:
-            await ctx.send("debug: no banner on user")
+            # await ctx.send("debug: no banner on user")
             custom_border = Image.open("media/rank/komi.jpg").convert("RGBA")
         custom_border = custom_border.resize(canvas_size)
         
